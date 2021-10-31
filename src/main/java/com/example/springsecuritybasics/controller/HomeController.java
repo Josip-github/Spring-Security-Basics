0af73a9dc39e8ae00ad156e80798234b9dbf8eab
Josip-github <josipcota1@gmail.com>
2021-10-31 15:54:58 +0100

    This commit completes HomeController class

--- src/main/java/com/example/springsecuritybasics/controller/HomeController.java
+++ src/main/java/com/example/springsecuritybasics/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.springsecuritybasics.controller;
 
+import com.example.springsecuritybasics.model.MessageForm;
 import com.example.springsecuritybasics.service.MessageListService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +19,16 @@ public class HomeController {
     }
 
     @GetMapping
-    public String getHomePage(Model model){
+    public String getHomePage(MessageForm messageForm, Model model){
         model.addAttribute("greetings", this.messageListService.getMessages());
+        return "home";
+    }
+
+    @PostMapping
+    public String addMessage(MessageForm messageForm, Model model){
+        this.messageListService.addMessage(messageForm.getText());
+        model.addAttribute("greetings", this.messageListService.getMessages());
+        messageForm.setText("");
         return "home";
     }
 }
