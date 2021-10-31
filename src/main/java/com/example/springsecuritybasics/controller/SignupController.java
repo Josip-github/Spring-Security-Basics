1c746320e202b2948d67b12afa61037229a505cd
Josip-github <josipcota1@gmail.com>
2021-10-31 13:40:17 +0100

    This commit creates SignupController class and the basic method signupView() that returns the name of the signup template

---
+++ src/main/java/com/example/springsecuritybasics/controller/SignupController.java
@@ -0,0 +1,23 @@
+package com.example.springsecuritybasics.controller;
+
+import com.example.springsecuritybasics.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController {
+
+    private final UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String signupView(){
+        return "signup";
+    }
+
+}
